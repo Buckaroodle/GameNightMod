bd2ca553959183c985cab3dcd3e2862411b5f881
Buckaroodle <aaron610@icloud.com>
2026-05-21 19:16:36 -0400

    v2 update
    Hungry Hippo:
    - added sprite

--- src/jokers/hungryhippo.lua
+++ src/jokers/hungryhippo.lua
@@ -2,8 +2,8 @@ SMODS.Joker {
     key = 'hungryhippo',
     atlas = 'bgn_joker_sprites',
     pos = {
-        x = 0,
-        y = 0
+        x = 1,
+        y = 6
     },
     config = {
         extra = {
